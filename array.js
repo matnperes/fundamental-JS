84ea644acb20a3ed53ccb6e23d98f9f527f2ab0c
matnperes <nobre_matheus@outlook.com>
2022-01-28 13:31:17 -0300

    for

--- array.js
+++ array.js
@@ -12,21 +12,66 @@
 // types.push({name: "Jane", age: 22, species: "human"})
 // console.table(types)
 
-let array = [0,,1,,,]
-console.log(array.length)
-console.log(array[1])
-console.log(array)
+// let array = [0,,1,,,]
+// console.log(array.length)
+// console.log(array[1])
+// console.log(array)
 
-let alunos = ["Matheus", "João", "Patricia", "Luana", "Jorge", "Pedro", "Mariana", "Laís"]
-let metade1 = []
-let metade2 = []
+// GET ELEMENTS DE ARRAY
 
-for(var i=0; i < alunos.length/2; i++){
-  metade1.push(alunos[i])
+// let students = ["Matheus", "João", "Patricia", "Luana", "Jorge", "Pedro", "Mariana", "Laís"]
+// let metade1 = [] HARD
+// let metade2 = []
+// for(var i=0; i < alunos.length/2; i++){
+//   metade1.push(alunos[i])
+// }
+// console.log(metade1)
+// for(var a= alunos.length/2; a <alunos.length; a++){
+//   metade2.push(alunos[a])
+// }
+// console.log(metade2)
+
+// let half1 = students.slice(0, students.length/2);
+//                    //start   //end              
+// let half2 = students.slice(students.length/2)
+
+// console.log(`${half1}\n${half2}`)
+
+// CHANGE ARRAY WITH SPLICE
+
+// let animals = ["gato", "peixe", "periquito", "baleia"]
+
+// Add more elements 
+// animals.splice(1, 0, "Tartaruga");
+//     where?, how many remove?, "What?"
+// console.log(animals)
+// animals.splice(1, 3, "elefante" );
+// console.log(animals)
+
+// CONCAT ARRAYS
+// let girls = ["Joana", "Patricia", "Rafaela", "Bianca"]
+// let boys = ["Pedro", "Gustavo", "Anderson", "Felipe"]
+
+// let all = girls.concat(boys)
+// console.log(all)
+
+//ARRAY TWO DIMENSIONS
+
+let id = [32421, 34511, 41241, 24112]
+let students = ["Spencer", "Carol", "Sam", "Rachel"]
+
+// console.log(`Hi, ${classRoom[1][2]}! your id in university is ${classRoom[0][2]}.  `)
+// console.log(students.includes("Spencer"))
+
+let classRoom = [id, students]
+// FIND IN ARRAYS
+
+const findStudent = (nameStudent) => {
+  if(classRoom[1].includes(nameStudent)){
+    let index = classRoom[1].indexOf(nameStudent)
+    return `Hi, ${classRoom[1][index]}! your id is ${classRoom[0][index]}`
+  }else{
+    return `Aluno não encontrado`
+  }
 }
-console.log(metade1)
-
-for(var a= alunos.length/2; a <alunos.length; a++){
-  metade2.push(alunos[a])
-}
-console.log(metade2)
+console.log(findStudent("Sam"));
