b9ff24c689046378f3c58f380294c5a79f0bea4b
matnperes <nobre_matheus@outlook.com>
2022-01-28 18:04:53 -0300

    Refactor reduce

--- reduce.js
+++ reduce.js
@@ -4,3 +4,10 @@ const somaTudo = numeros.reduce(function(acum, atual){
   return atual + acum
 },0)
 console.log(somaTudo)
+
+//A função de callback pode ser declarada em reduce de 3 maneiras
+// Dentro da função através por arrow function ou função anonima e fora da função e apenas declarar.  
+
+// function soma(acum, atual){
+//   return atual + acum 
+// } 
